b5ceafc0ac1f2a2621995f83a91751296da2b1c3
norareidy <nora.reidy@mongodb.com>
2025-01-15 12:42:57 -0500

    more

--- source/includes/interact-data/specify-a-query.py
+++ source/includes/interact-data/specify-a-query.py
@@ -2,6 +2,14 @@
 from django.db import models
 from django_mongodb_backend.fields import EmbeddedModelField, ArrayField
 
+class Award(models.Model):
+    wins = models.IntegerField(default=0)
+    nominations = models.IntegerField(default=0)
+    text = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+
 class Movie(models.Model):
     title = models.CharField(max_length=200)
     plot = models.TextField(blank=True)
@@ -16,14 +24,6 @@ class Movie(models.Model):
     
     def __str__(self):
         return self.title
-    
-class Award(models.Model):
-    wins = models.IntegerField(default=0)
-    nominations = models.IntegerField(default=0)
-    text = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
 # end-models
 
 # start-all
@@ -45,3 +45,27 @@ Movie.objects.exclude(released__lt=timezone.make_aware(datetime(1980, 1, 1)))
 # start-filter-contains
 Movie.objects.filter(plot__contains="coming-of-age")
 # end-filter-contains
+
+# start-filter-lte
+Movie.objects.filter(runtime__lte=50)
+# end-filter-lte
+
+# start-filter-relationships
+Movie.objects.filter(awards__wins=93)
+# end-filter-relationships
+
+# start-filter-combine
+Movie.objects.filter(awards__text__istartswith="nominated")
+# end-filter-combine
+
+# start-sort
+Movie.objects.filter(title__startswith="Rocky").order_by("released")
+# end-sort
+
+# start-limit
+Movie.objects.filter(released=timezone.make_aware(datetime(2010, 7, 1)))[3:6]
+# end-limit
+
+# start-first
+Movie.objects.filter(genres=["Crime", "Comedy"]).first()
+# end-first
