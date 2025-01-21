2a91d9620798223b41c8ec3e09bb20b177d69cbf
norareidy <nora.reidy@mongodb.com>
2025-01-21 16:45:56 -0500

    more info

--- source/includes/model-data/models.py
+++ source/includes/model-data/models.py
@@ -15,6 +15,24 @@ class Movie(models.Model):
         return self.title
 # end-models
 
+# start-json-field
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    plot = models.TextField(blank=True)
+    runtime = models.IntegerField(default=0)
+    released = models.DateTimeField("release date", null=True, blank=True)
+    imdb = models.JSONField(null=True, blank=True)
+
+    class Meta:
+        db_table = "movies"
+        managed = False
+    
+    def __str__(self):
+        return self.title
+# end-json-field
+
 # start-array-field
 from django.db import models
 from django_mongodb_backend.fields import ArrayField
