108db9246b55226f5fcda82ae5123c4780db0980
norareidy <nora.reidy@mongodb.com>
2025-01-22 15:32:20 -0500

    url

--- source/includes/get-started/urls.py
+++ source/includes/get-started/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
     path("recent_movies/", views.recent_movies, name="recent_movies"),
     path("viewers_list/", views.viewers_list, name="viewers_list"),
+    path("", views.index, name="index"),
 ]
